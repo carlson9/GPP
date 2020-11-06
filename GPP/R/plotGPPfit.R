2ba6a722aec2e20446c4148c486f3818142a95e7
devinpbrown96 <devinpbrown96@gmail.com>
2020-11-06 18:23:58 +0300

    File ready for final review

--- GPP/R/plotGPPfit.R
+++ GPP/R/plotGPPfit.R
@@ -1,1 +1,65 @@
 #plots the results of a (converged) model, with true and projected
+#' Plots results of a (converged) model, with true and projected values.
+#'
+#' Takes the results of a Gaussian Process Projection fit and generates a linear plot of the actual and predicted counterfactual values
+#'
+#' @param fit The fit results of the GPP stan model.
+#' @param df The dataframe used in your model. 
+#' @param obvColName The column name that includes your observation of interest. Must be a string.
+#' @param obvName  The name of the specific observation of interest. Must be a string. 
+#' @param expc The explanatory variable that is subjected to the counterfactual claim.
+#' @param starttime The start time of the treatment effect.
+#' @param timeColName The name of the column that includes your time variable. 
+#' @param legendLoc The preferred location of the legend in the final graph. Defaults to "topleft". 
+#' @param xlabel The label of the x-axis in the final graph. Defaults to input for 'timeColName'.
+#' @param ylabel The preferred label of the y-axis in the final graph. Defaults to input for 'expv'.
+#' @param actualdatacol The preferred color for plotted line for actual data. Defaults to black. 
+#' @param predatacol The preferred color for plotted line for predicted counterfactual data. Defaults to red. 
+#' 
+#' @return A plot built in r-base
+#' 
+#' @author Devin P. Brown \email{devinpbrown96@gmail.com} and David G. Carlson \email{carlson.david@@wustl.edu} 
+#' @examples
+#' 
+#' fit = 
+#' df = 
+#' 
+#' plotGPPfit(fit=fit, df=d, obvColName='country', obvName='West Germany',expv='gdp', starttime=1990, timeColName='year', legendLoc='bottomright', xlabel="Test X-Label", ylabel = "Test Y-Label", actualdatacol = 'blue', preddatacol = 'green')
+#' 
+#' 
+#' @seealso \code{\link{autoConverge}} \code{\link{GPP}} \code{\link{runMod}} \code{\link{writeMod}}
+#' @rdname plotGPPfit
+#' @aliases plotGPPfit,ANY-method
+#' @export
+setGeneric(name="runLM",
+           def=function(fit, df, obvColName, obvName, expv, starttime, timeColName, legendLoc = 'topleft', xlabel=NULL, ylabel=NULL, actualdatacol = 'black', preddatacol = 'red',...)
+           {standardGeneric("plotGPPfit")}
+)
+
+#' @export
+setMethod(f="plotGPPfit",
+          definition=function(fit, df, obvColName, obvName, expv, starttime, timeColName, legendLoc = 'topleft', xlabel=NULL, ylabel=NULL, actualdatacol = 'black', preddatacol = 'red',...){
+            if (is.null(xlabel)) xlabel = timeColName
+            if (is.null(ylabel)) ylabel = expv
+            ys = as.numeric(df[,expv])
+            ys2 = ys
+            ys2[df[,obvColName] == obvName & df[, timeColName] > starttime] = NA
+            timelength = 1:(max(df[, timeColName])-starttime+1)
+            ys3 = c(ys, summary(fit)$summary[paste0('ystar[', timelength, ']'), '97.5%']*sd(as.numeric(na.omit(ys2))) + mean(as.numeric(na.omit(ys2))), summary(fit)$summary[paste0('ystar[', timelength, ']'), '2.5%']*sd(as.numeric(na.omit(ys2))) + mean(as.numeric(na.omit(ys2))))
+            
+            
+            plot(df[,expv][df[,obvColName]==obvName] ~
+                   df[,timeColName][df[,obvColName]==obvName],
+                 type = 'l', xlab = xlabel, ylab = paste('(Predicted)',ylabel),
+                 ylim = c(min(ys3), max(ys3)), col=actualdatacol, ...)
+            lines(starttime:max(df[,timeColName]), summary(fit)$summary[paste0('ystar[', timelength, ']'), 'mean']*sd(as.numeric(na.omit(ys2))) + mean(as.numeric(na.omit(ys2))),
+                  type='l', col=preddatacol)
+            lines(starttime:max(df[,timeColName]), summary(fit)$summary[paste0('ystar[', timelength, ']'), '2.5%']*sd(as.numeric(na.omit(ys2))) + mean(as.numeric(na.omit(ys2))),
+                  type='l', col=preddatacol, lty=2)
+            lines(starttime:max(df[,timeColName]), summary(fit)$summary[paste0('ystar[', timelength, ']'), '97.5%']*sd(as.numeric(na.omit(ys2))) + mean(as.numeric(na.omit(ys2))),
+                  type='l', col=preddatacol, lty=2)
+            legend(legendLoc , legend = c(ylabel, paste('Predicted', ylabel)), col = c(actualdatacol, preddatacol), lty = 1, bty = 'n')
+            }
+)
+          
+          
