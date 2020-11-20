146f0b2228597f8a0f8acb0af73ba6a75d7015a1
carlson9 <dcarlson@ku.edu.tr>
2020-11-20 12:51:15 +0300

    development file added

---
+++ development.R
@@ -0,0 +1,22 @@
+## Load libraries and set working directory
+library(devtools)
+library(roxygen2)
+library(jsonlite)
+library(httr)
+library(plyr)
+library(testthat)
+
+
+setwd("~/GPP")
+# This will need to be changed to match your directory of where the package is.
+## This is run once when the package strcuture is first created
+
+
+## At this point put the *.R files into the correct directories and edit the DESCRIPTION file
+current.code <- as.package("GPP")
+load_all(current.code)
+document(current.code)
+check(current.code)
+build(current.code)
+
+
